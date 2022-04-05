6face1c5155909333051bf3447d6714809b9835b
Eduardo Leal <eduardo.leeal@hotmail.com>
2022-04-04 21:32:56 -0300

    Update
    Mostra a situação da pessoa

--- all files/main.py
+++ all files/main.py
@@ -4,5 +4,10 @@ import scripts
 
 altura = str(input("Digite sua altura em CM: "))
 peso = str(input("Digite seu peso: "))
+sexo = str(input("Digite seu genêro: "))
+idade = int(input("Digite sua idade: "))
 
-print(f'IMC: {scripts.calculo_imc(altura, peso):.5f}')
+imc = scripts.calculo_imc(altura, peso)
+print(f'IMC: {imc:.5f}')
+situação = scripts.result_imc(sexo, imc, idade)
+print(f'{situação}')
--- all files/scripts.py
+++ all files/scripts.py
@@ -9,3 +9,32 @@ def calculo_imc(alt = 0, pes = 0):
         return (imc)
     else:
         return("Parametros Incorretos!")
+    
+def result_imc(gen, resIMC, idade):
+    """
+    :param gen: Genero da Pessoa
+    :param resIMC: Resultado do calculo IMC
+    :param idade: Idade da pessoa
+    :return: Retorna a situação da pessoa"""
+    if(gen.strip().upper()[0] == "M"):
+        if (resIMC < 20.7):
+            return("Abaixo do peso")
+        if (resIMC >= 20.7 and resIMC < 26.4):
+            return("Peso ideal")
+        if (resIMC >= 26.5 and resIMC < 27.8):
+            return("Pouco acima do peso")
+        if (resIMC >= 27.9 and resIMC < 31.1):
+            return("Acima do peso")
+        if (resIMC >= 31.2):
+            return"Obesidade"
+    if(gen.strip().upper()[0] == "F"):
+            if (resIMC < 19.1):
+                return("Abaixo do peso")
+            if (resIMC >= 19.1 and resIMC < 25.8): 
+                return("Peso ideal")
+            if (resIMC >= 25.9 and resIMC < 27.3):
+                return("Pouco acima do peso")
+            if (resIMC >= 27.4 and resIMC < 32.3):
+                return("Acima do peso")
+            if (resIMC >= 32.4):
+                return('Obesidade')
