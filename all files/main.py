f44fe1ba845de009e6ae74ae0297471895556736
Eduardo Leal <eduardo.leeal@hotmail.com>
2022-04-04 21:41:43 -0300

    Update main.py

--- all files/main.py
+++ all files/main.py
@@ -1,13 +1,12 @@
 import scripts
 
-
-
 altura = str(input("Digite sua altura em CM: "))
 peso = str(input("Digite seu peso em Kg: "))
 sexo = str(input("Digite seu sexo: "))
 idade = str(input("Digite sua idade: "))
 
 imc = scripts.calculo_imc(altura, peso)
-print(f'IMC: {imc:.5f}')
 situação = scripts.result_imc(sexo, imc, idade)
-print(f'{situação}')
+
+print(f'IMC: {imc:.5f}Kg/m²')
+print(f'Situação: {situação}')
